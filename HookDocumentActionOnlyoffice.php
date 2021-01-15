993aa5da85109a9595bf2775f70941ccdd344d7d
Antipkin-A <Artem.Antipkin@onlyoffice.com>
2021-01-15 19:58:25 +0300

    added class HookDocumentActionOnlyoffice

---
+++ HookDocumentActionOnlyoffice.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__.'/onlyoffice_tools.php';
+
+class HookDocumentActionOnlyoffice extends HookObserver implements HookDocumentActionObserverInterface
+{
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        parent::__construct(
+            'plugin/onlyoffice/onlyoffice_plugin.php',
+            'onlyoffice'
+        );
+    }
+
+    /**
+     * Create a Onlyoffice edit tools when the Chamilo loads document tools
+     *
+     * @param HookDocumentActionEventInterface $event - the hook event
+     */
+    public function notifyDocumentAction(HookDocumentActionEventInterface $event) {
+        $data = $event->getEventData();
+
+        if ($data['type'] === HOOK_EVENT_TYPE_PRE) {
+            $data['actions'][] = OnlyofficeTools::getButtonCreateNew();
+            return $data;
+        }
+    }
+}
